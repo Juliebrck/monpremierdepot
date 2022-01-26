15a237927384b5de1391b031c10cd65b016f0de2
Julie Bricka <bricka.j@gmail.com>
2022-01-26 10:52:35 +0100

    my first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const contacts = [
+    {
+        prenom: "John",
+        email: "john@gmail.com"
+    },
+    {
+        prenom: "Vanessa",
+        email: "vanessa@gmail.com"
+    },
+    {
+        prenom: "Thomas",
+        email: "thomas@gmail.com"
+    },
+];
+
+for (let i = 0; i < contacts.length; i++) {
+    console.log("prénom", contacts[i].prenom);
+    if (contacts[i].prenom === 'john') {
+        let email = contacts[i].email
+    }
+    console.log(email)
+}
